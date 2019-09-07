5e1f3c79e2aec76da48004bffde283007762811d
Andreas Menge <andi.menge@gmail.com>
2019-09-07 22:58:42 +0200

    add slack notifier and fix linting

--- src/slack/slack.ts
+++ src/slack/slack.ts
@@ -1,13 +1,26 @@
-import { IncomingWebhook } from "@slack/webhook";
+import { IncomingWebhook } from '@slack/webhook'
+import { IslackMsg } from './slackModel'
 
-const webhookURL = process.env.SLACK_WEBHOOK_URL;
-const webhook = new IncomingWebhook(webhookURL);
+const webhookURL = process.env.SLACK_WEBHOOK_URL
+const webhook = new IncomingWebhook(webhookURL)
 
-export async function sendToSlack(msg: object) {
+export async function sendToSlack(title: string, imdbID: string, imageURL: string) {
+  const msg: IslackMsg = {
+    attachments: [
+      {
+        fallback: `watch ${title}`,
+        pretext: 'Watch this:',
+        image_url: imageURL,
+        title: title,
+        title_link: `https://www.imdb.com/title/${imdbID}`,
+      },
+    ],
+  }
+
   try {
-    await webhook.send(msg);
+    await webhook.send(msg)
   } catch (error) {
-    console.error(error.message);
-    throw new Error("Could not send slack message");
+    console.error(error.message)
+    throw new Error('Could not send slack message')
   }
 }
