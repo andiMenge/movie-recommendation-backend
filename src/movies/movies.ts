74782c7bb0170f05b84bebbeb537d1a4bce06b58
Andreas Menge <andi.menge@gmail.com>
2019-09-24 19:03:31 +0200

    re-write tmdb withou ES6 classes

--- src/movies/movies.ts
+++ src/movies/movies.ts
@@ -1,24 +1,23 @@
 import * as mongoose from 'mongoose'
 import { MovieSchema, MovieModel } from './moviesModels'
 import { Movieresult } from '../tmdb/tmdbModels'
-import Tmdb from '../tmdb/tmdb'
 import { isDuplicate } from './moviesHelpers'
 import { sendToSlack } from '../slack/slack'
 import { addToFeed } from '../feed/feed'
+import { getMovieInfo, getGenreNamesfromGenreIds, tmdbThumbnailURL } from '../tmdb/tmdb'
 
 const movie = mongoose.model<MovieModel>('Movie', MovieSchema)
-const tmdb = new Tmdb()
 
 export async function saveMovie(id: string): Promise<MovieModel> {
   try {
-    const movieInfo: Movieresult = await tmdb.getMovieInfo(id)
-    const genres = await tmdb.getGenreNamesfromGenreIds(movieInfo.genre_ids)
+    const movieInfo: Movieresult = await getMovieInfo(id)
+    const genres = await getGenreNamesfromGenreIds(movieInfo.genre_ids)
     const newMovie = new movie({
       original_title: movieInfo.title,
       imdb_id: id,
       release_date: movieInfo.release_date,
       is_highlight: false,
-      image_url: `${tmdb.tmdbThumbnailURL}${movieInfo.poster_path}`,
+      image_url: `${tmdbThumbnailURL}${movieInfo.poster_path}`,
       genres: genres,
     })
 
--- src/tmdb/tmdb.ts
+++ src/tmdb/tmdb.ts
@@ -2,59 +2,47 @@ import needle from 'needle'
 import { tmdbFindMovieResponse, Movieresult, TmdbGenres, Genre } from '../tmdb/tmdbModels'
 import { config } from '../config/config'
 
-export default class Tmdb {
-  tmdbUrl: string
-  tmdbEndpoint: string
-  tmdbApiKey: string
-  tmdbThumbnailURL: string
+const tmdbUrl = 'https://api.themoviedb.org/3/'
+const tmdbEndpoint = 'find'
+const tmdbApiKey = config.get('tmdb.token')
+export const tmdbThumbnailURL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
 
-  constructor() {
-    this.tmdbUrl = 'https://api.themoviedb.org/3/'
-    this.tmdbEndpoint = 'find'
-    this.tmdbApiKey = config.get('tmdb.token')
-    this.tmdbThumbnailURL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'
+export async function getMovieInfo(imdbID: String): Promise<Movieresult> {
+  const url = `${tmdbUrl}${tmdbEndpoint}/${imdbID}?api_key=${tmdbApiKey}&language=en-US&external_source=imdb_id`
+  try {
+    const resp = await needle('get', url)
+    const tmdbResponse: tmdbFindMovieResponse = resp.body
+    const tmdbMovieDetails: Movieresult = tmdbResponse.movie_results[0]
+    return tmdbMovieDetails
+  } catch (error) {
+    throw new Error(error.message)
   }
+}
 
-  public async getMovieInfo(imdbID: String): Promise<Movieresult> {
-    const url = `${this.tmdbUrl}${this.tmdbEndpoint}/${imdbID}?api_key=${this.tmdbApiKey}&language=en-US&external_source=imdb_id`
-    try {
-      const resp = await needle('get', url)
-      const tmdbResponse: tmdbFindMovieResponse = resp.body
-      const tmdbMovieDetails: Movieresult = tmdbResponse.movie_results[0]
-      return tmdbMovieDetails
-    } catch (error) {
-      console.log(error.message)
-      throw new Error('get movie info from tmdb failed')
-    }
+export async function getGenres(): Promise<Genre[]> {
+  const url = `${tmdbUrl}genre/movie/list?api_key=${tmdbApiKey}`
+  try {
+    const resp = await needle('get', url)
+    const TmdbRespBody: TmdbGenres = resp.body
+    return TmdbRespBody.genres
+  } catch (error) {
+    throw new Error(error.message)
   }
+}
 
-  public async getGenres(): Promise<Genre[]> {
-    const url = `${this.tmdbUrl}genre/movie/list?api_key=${this.tmdbApiKey}`
-    try {
-      const resp = await needle('get', url)
-      const TmdbRespBody: TmdbGenres = resp.body
-      return TmdbRespBody.genres
-    } catch (error) {
-      console.log(error.message)
-      throw new Error('get genres from tmdb failed')
-    }
-  }
-
-  public async getGenreNamesfromGenreIds(genre_ids: number[]): Promise<string[]> {
-    const genreNames: string[] = []
-    try {
-      const tmdbGenres: Genre[] = await this.getGenres()
-      for (const genreId of genre_ids) {
-        for (const tmdbGenre of tmdbGenres) {
-          if (genreId === tmdbGenre.id) {
-            genreNames.push(tmdbGenre.name)
-          }
+export async function getGenreNamesfromGenreIds(genre_ids: number[]): Promise<string[]> {
+  const genreNames: string[] = []
+  try {
+    const tmdbGenres: Genre[] = await getGenres()
+    for (const genreId of genre_ids) {
+      for (const tmdbGenre of tmdbGenres) {
+        if (genreId === tmdbGenre.id) {
+          genreNames.push(tmdbGenre.name)
         }
       }
-      return genreNames
-    } catch (error) {
-      console.log(error.message)
-      throw new Error('could not compute genre names')
     }
+    return genreNames
+  } catch (error) {
+    throw new Error(error.message)
   }
 }
